077264a303993dbb89170fc77e6bd95f923871bf
Vunky Himawan <tingkerbeii44@gmail.com>
2025-09-04 14:52:31 +0700

    feat: enhance CORS configuration with allowed methods and headers

--- api/app/src/lib.rs
+++ api/app/src/lib.rs
@@ -1,4 +1,10 @@
-use axum::{Router, http::HeaderValue};
+use axum::{
+    Router,
+    http::{
+        HeaderValue, Method,
+        header::{AUTHORIZATION, CONTENT_TYPE},
+    },
+};
 
 pub mod v1;
 use common::AppState;
@@ -13,7 +19,16 @@ pub async fn build_routes(db: Database) -> Router {
 
     let origin = HeaderValue::from_str(&app_config.url).expect("invalid origin URL in AppConfig");
 
-    let cors_layer = CorsLayer::new().allow_origin(origin);
+    let cors_layer = CorsLayer::new()
+        .allow_origin(origin)
+        .allow_methods([
+            Method::GET,
+            Method::POST,
+            Method::PUT,
+            Method::DELETE,
+            Method::OPTIONS,
+        ])
+        .allow_headers([CONTENT_TYPE, AUTHORIZATION]);
 
     Router::new()
         .nest("/api", create_routes().await)
