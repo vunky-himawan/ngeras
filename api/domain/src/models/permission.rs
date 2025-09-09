a48b8165418cd6e31035dd80c6784172a75b2455
Vunky Himawan <tingkerbeii44@gmail.com>
2025-09-09 11:22:44 +0700

    feat: enhance permissions API with additional endpoints and documentation

--- api/domain/src/models/permission.rs
+++ api/domain/src/models/permission.rs
@@ -1,7 +1,8 @@
 use serde::{Deserialize, Serialize};
 use sqlx::prelude::FromRow;
+use utoipa::ToSchema;
 
-#[derive(Serialize, Debug, Deserialize, Default, FromRow)]
+#[derive(Serialize, Debug, Deserialize, Default, FromRow, ToSchema)]
 pub struct Permission {
     pub id: i64,
     pub name: String,
